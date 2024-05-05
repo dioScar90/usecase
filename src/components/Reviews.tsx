e37bf8799c1dc9747dd62c2b202fffb6b1a18374
Diogo L Scarmagnani <diogols@live.com>
2024-05-05 20:08:17 -0300

    landing page ready until footer, but problems with animation

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -15,31 +15,31 @@ const PHONES = [
   '/testimonials/6.jpg',
 ]
 
+// function splitArray<T>(arr: Array<T>, numParts: number) {
+//   return arr.reduce((acc: Array<Array<T>>, curr: T, i) => {
+//     const index = i % numParts
+
+//     acc[index] ??= []
+//     acc[index].push(curr)
+
+//     return acc
+//   }, [])
+// }
+
 function splitArray<T>(arr: Array<T>, numParts: number) {
-  return arr.reduce((acc: Array<Array<T>>, curr: T, i) => {
+  const result: Array<Array<T>> = []
+
+  for (let i = 0; i < arr.length; i++) {
     const index = i % numParts
 
-    acc[index] ??= []
-    acc[index].push(curr)
+    if (!result[index]) {
+      result[index] = []
+    }
 
-    return acc
-  }, [])
-}
+    result[index].push(arr[i])
+  }
 
-interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
-  imgSrc: string
-}
-
-const Review = ({ imgSrc, className, ...props }: ReviewProps) => {
-  const POSSIBLE_ANIMATION_DELAYS = ['0s', '0.1s', '0.2s', '0.3s', '0.4s', '0.5s']
-  const animationDelay = POSSIBLE_ANIMATION_DELAYS[Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)]
-  console.log('animationDelay', animationDelay)
-
-  return (
-    <div className={cn('animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5', className)} style={{ animationDelay } as CSSProperties} {...props}>
-      <Phone imgSrc={imgSrc} />
-    </div>
-  )
+  return result
 }
 
 const ReviewColumn = ({
@@ -48,21 +48,21 @@ const ReviewColumn = ({
   reviewClassName,
   msPerPixel = 0
 }: {
-  reviews: string[],
-  className?: string,
-  reviewClassName?: (reviewIndex: number) => string,
+  reviews: string[]
+  className?: string
+  reviewClassName?: (reviewIndex: number) => string
   msPerPixel?: number
 }) => {
   const columnRef = useRef<HTMLDivElement | null>(null)
   const [columnHeight, setColumnHeight] = useState(0)
-  const duration = `${columnHeight * msPerPixel}ms`
+  const duration = (columnHeight * msPerPixel) + 'ms'
 
   useEffect(() => {
     if (!columnRef.current) {
       return
     }
 
-    const resizeObserver = new ResizeObserver(() => {
+    const resizeObserver = new window.ResizeObserver(() => {
       setColumnHeight(columnRef.current?.offsetHeight ?? 0)
     })
 
@@ -72,10 +72,46 @@ const ReviewColumn = ({
   }, [])
 
   return (
-    <div ref={columnRef} className={cn('animate-marquee space-y-8 py-4', className)} style={{'--marquee-duration': duration} as CSSProperties}>
-      {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
-        <Review key={reviewIndex} className={reviewClassName?.(reviewIndex % reviews.length)} imgSrc={imgSrc} />
-      ))}
+    <div
+      ref={columnRef}
+      className={cn('animate-marquee space-y-8 py-4', className)}
+      style={{ '--marquee-duration': duration } as CSSProperties}
+    >
+      {reviews.concat(reviews)
+        .map((imgSrc, reviewIndex) => (
+          <Review
+            key={reviewIndex}
+            className={reviewClassName?.(reviewIndex % reviews.length)}
+            imgSrc={imgSrc}
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string
+}
+
+const Review = ({ imgSrc, className, ...props }: ReviewProps) => {
+  const POSSIBLE_ANIMATION_DELAYS = ['0s', '0.1s', '0.2s', '0.3s', '0.4s', '0.5s']
+  
+  const animationDelay =
+    POSSIBLE_ANIMATION_DELAYS[
+      Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
+    ]
+  
+  return (
+    <div
+      className={cn(
+        'animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5',
+        className
+      )}
+      style={{ animationDelay } as CSSProperties}
+      {...props}
+    >
+      <Phone imgSrc={imgSrc} />
     </div>
   )
 }
@@ -89,7 +125,10 @@ const ReviewGrid = () => {
   const column3 = splitArray(columns[2], 2)
 
   return (
-    <div ref={containerRef} className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
       {isInView ? (
         <>
           <ReviewColumn
@@ -105,7 +144,9 @@ const ReviewGrid = () => {
           <ReviewColumn
             reviews={[...column2, ...column3[1]]}
             className="hidden md:block"
-            reviewClassName={(reviewIndex) => reviewIndex >= column2.length ? 'lg:hidden' : ''}
+            reviewClassName={(reviewIndex) =>
+              reviewIndex >= column2.length ? 'lg:hidden' : ''
+            }
             msPerPixel={15}
           />
           <ReviewColumn
@@ -125,7 +166,12 @@ const ReviewGrid = () => {
 export const Reviews = () => {
   return (
     <MaxWidthWrapper className="relative max-w-5xl">
-      <img src="/what-people-are-buying.png" alt="What people are buying" className="absolute select-none hidden xl:block -left-32 top-1/3" aria-hidden="true" />
+      <img
+        aria-hidden="true"
+        src="/what-people-are-buying.png"
+        alt="What people are buying"
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
 
       <ReviewGrid />
     </MaxWidthWrapper>
