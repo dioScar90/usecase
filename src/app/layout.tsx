b00e1eab24bc91f30b10678fcdee7c0c4190cc4e
Diogo L Scarmagnani <diogols@live.com>
2024-05-26 10:59:31 -0300

    grainy background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body className={recursive.className}>
         <Navbar />
 
-        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">
             <Providers>
               {children}
